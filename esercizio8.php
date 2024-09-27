f7d9a8cc0f657ba8f335cff4ebf8dd92b13feddc
Benncio09 <lorebencini13@outlook.com>
2024-09-27 08:36:49 +0200

    esercizio finito e funzionante

--- esercizio8.php
+++ esercizio8.php
@@ -6,6 +6,25 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+        $modello="samsung";
+        $grado="a"; //a, b o c
+        switch($grado){
+            case "a":
+                $str="<h2> $modello perfetto, pari al nuovo</h2>";
+                break;
+            case "b":
+                $str="<h2> $modello in ottime condizioni</h2>";
+                break;
+            case "c":
+                $str="<h2> $modello con graffi e/o segni di usura</h2>";
+                break;
+            default:
+                $str="<h2 style='color:red'> errore: grado non riconosciuto</h2>";
+                break;
+        }
+        echo $str;
+
+    ?>
 </body>
 </html>
